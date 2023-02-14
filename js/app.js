c8b5a9a91cc3674873ec752a1bed49699d13e2b8
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-14 15:16:59 +0100

    FEAT array of obj created

---
+++ js/app.js
@@ -0,0 +1,47 @@
+const member = {
+  firstName: '',
+  lastName: '',
+  role: '',
+  profileImg: '',
+}
+
+const teamMembers = [
+  member,
+  {
+    firstName: 'Wayne',
+    lastName: 'Barnett',
+    role: 'Founder & CEO',
+    profileImg: './img/wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    firstName: 'Angela',
+    lastName: 'Caroll',
+    role: 'Chief Editor',
+    profileImg: './img/angela-caroll-chief-editor.jpg',
+  },
+  {
+    firstName: 'Wayne',
+    lastName: 'Barnett',
+    role: 'Founder & CEO',
+    profileImg: './img/walter-gordon-office-manager.jpg',
+  },
+  {
+    firstName: 'Wayne',
+    lastName: 'Barnett',
+    role: 'Founder & CEO',
+    profileImg: './img/angela-lopez-social-media-manager.jpg',
+  },
+  {
+    firstName: 'Wayne',
+    lastName: 'Barnett',
+    role: 'Founder & CEO',
+    profileImg: 'scott-estrada-developer.jpg',
+  },
+  {
+    firstName: 'Wayne',
+    lastName: 'Barnett',
+    role: 'Founder & CEO',
+    profileImg: './img/barbara-ramos-graphic-designer.jpg',
+  },
+
+]
